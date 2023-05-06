76bb80e1282b3e2efaebd8bae31bb54a3f57b7c9
Jlee108 <jbacked@yahoo.com>
2023-05-06 09:56:09 +0100

    initial commit

---
+++ src/main/java/CurrencyConverter/Currency.java
@@ -0,0 +1,15 @@
+package CurrencyConverter;
+
+import java.util.HashMap;
+
+public class Currency {
+    //hashmap for currency mapping to country
+    HashMap<String, Double> countryCurrency;
+    public Currency() {
+        this.countryCurrency = new HashMap<>();
+    }
+
+    public void add(String country, Double currency) {
+        countryCurrency.put(country, currency);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import CurrencyConverter.Currency;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Currency currency = new Currency();
+        currency.add("US", 1.00);
+        currency.add("UK", 0.87);
+
+        System.out.println(currency);
+
+
+    }
+
+}
