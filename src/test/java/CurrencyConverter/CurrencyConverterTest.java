2f68071f4d45295580bdc67b9d28747745075c64
Michael Fernandez <mfernandezm85@gmail.com>
2023-05-22 12:31:29 -0400

    Hamcrest testing dependency

--- src/test/java/CurrencyConverter/CurrencyConverterTest.java
+++ src/test/java/CurrencyConverter/CurrencyConverterTest.java
@@ -2,18 +2,30 @@ package CurrencyConverter;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.hasKey;
+
 class CurrencyConverterTest {
 
 
     @Test
-    void isAddSuccess() {
+    public void isAddSuccess() {
         //given
         CurrencyConverter addMethod = new CurrencyConverter();
 
         //when
-        addMethod.addCountryAndExchangeRate("UDS", 1.00);
+        addMethod.addCountryAndExchangeRate("USD", 1.00);
 
         //then
+        assertThat(addMethod, hasEntry("USD", 1.00));
+
+//        Map<String, String> map = new HashMap<>();
+//        map.put("mike", "jag");
+//        assertThat(map, hasEntry("mike", "jag"));
     }
 
 }
